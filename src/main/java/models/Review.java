ab2113b1d52683ebda5c833830106dc074bda210
benruwel <benruwel21@gmail.com>
2020-07-14 19:10:53 +0300

    initial commit

---
+++ src/main/java/models/Review.java
@@ -0,0 +1,80 @@
+package models;
+
+public class Review {
+    private String content;
+    private String writtenBy;
+    private int rating;
+    private int id;
+    private int restaurantId; //will be used to connect Restaurant to Review (one-to-many)
+
+    public Review(String content, String writtenBy, int rating, int restaurantId) {
+        this.content = content;
+        this.writtenBy = writtenBy;
+        this.rating = rating;
+        this.restaurantId = restaurantId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Review review = (Review) o;
+
+        if (rating != review.rating) return false;
+        if (id != review.id) return false;
+        if (restaurantId != review.restaurantId) return false;
+        if (!content.equals(review.content)) return false;
+        return writtenBy.equals(review.writtenBy);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = content.hashCode();
+        result = 31 * result + writtenBy.hashCode();
+        result = 31 * result + rating;
+        result = 31 * result + id;
+        result = 31 * result + restaurantId;
+        return result;
+    }
+}
